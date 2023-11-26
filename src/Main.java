d57b9177f6818804945f1c67e5e93350338e24f7
Xavier Anchapaxi <canchapaxi6443@utm.edu.ec>
2023-11-26 18:40:53 -0500

    cuarto commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<ImpactoEcologico> lista_objetos = new ArrayList<>();
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Crear objetos de cada clase y agregarlos al ArrayList
+        lista_objetos.add(new Edificio("Edificio Residecial", 22300));
+        lista_objetos.add(new Auto("Xplorer", 225));
+        lista_objetos.add(new Bicicleta("Bici Personal", false));
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
+
